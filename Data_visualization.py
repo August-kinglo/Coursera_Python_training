6c2dd452f25e957226bd92f3fc28429813b7d42d
August Abdon KINGLO <113606354+August-kinglo@users.noreply.github.com>
2023-09-19 13:30:26 +0200

    Update Data_visualization.py

--- Data_visualization.py
+++ Data_visualization.py
@@ -1,7 +1,1 @@
-def style (style):
-  #The purpose of this function is to set the style of my graph directly
-  plt.style.use(style)
 
-def plot_type (type):
-  #The purpose of this function is to set the type of plot I intend to plot directly
-  data.plot(kind=type)
